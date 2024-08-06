3895f336c121b1465796b53ee6e1953b1f03d2a6
RockieBoy <changesmoothyman04@gmail.com>
2024-08-06 12:40:23 +0700

    edit perubahan sedikit

--- resources/views/content/dashboard.blade.php
+++ resources/views/content/dashboard.blade.php
@@ -13,6 +13,7 @@ Coded by www.creative-tim.com
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 -->
       <!-- End Navbar -->
+<php
 @extends('layouts.main')
 @section('content')
       <div class="content">
